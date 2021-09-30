20c144094362ea554de7fd95e2b80ef2dc47e5a9
globotix <denesh.siva@globotix.sg>
2021-10-01 02:51:33 +0800

    Changed shebang to python3, formatted code to use spaces only, changed print format

--- examples/custom_client.py
+++ examples/custom_client.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import time
 import locale
@@ -27,10 +27,10 @@ name = "pyDice Example"
 server = "sandbox.spacebrew.cc"
 
 for cur_ele in sys.argv:
-	if "name" in cur_ele: 
-		name = cur_ele[5:]
-	if "server" in cur_ele: 
-		server = cur_ele[7:]
+    if "name" in cur_ele:
+        name = cur_ele[5:]
+    if "server" in cur_ele:
+        server = cur_ele[7:]
 
 # configure the spacebrew client
 brew = Spacebrew(name=name, server=server)
@@ -38,59 +38,65 @@ brew.addPublisher("roll of the dice", "dice")
 brew.addSubscriber("what did you roll", "dice")
 
 # function that handles the incoming spacebrew dice messages
+
+
 def handleDice(value):
-	global code, stdscr
-	stdscr.addstr(pos_remote, pos_state, (" " * 30).encode(code))
-	if value < 1 or value > 6:
-		stdscr.addstr(pos_remote, pos_state, ("you rolled a " + str(value) + "! What kind of dice is that? ").encode(code))
-	else: 
-		stdscr.addstr(pos_remote, pos_state, str(value).encode(code))
-	stdscr.refresh()
+    global code, stdscr
+    stdscr.addstr(pos_remote, pos_state, (" " * 30).encode(code))
+    if value < 1 or value > 6:
+        stdscr.addstr(pos_remote, pos_state, ("you rolled a " +
+                      str(value) + "! What kind of dice is that? ").encode(code))
+    else:
+        stdscr.addstr(pos_remote, pos_state, str(value).encode(code))
+    stdscr.refresh()
+
 
 # register handler function with appropriate subscription data feed
 brew.subscribe("what did you roll", handleDice)
 
 try:
-	# start-up spacebrew
-	brew.start()	
+    # start-up spacebrew
+    brew.start()
 
-	# create and load info message at the top of the terminal window
-	info_msg = "This is the pySpacebrew library custom data type example. It rolls the dice every time the enter or return\n" 
-	info_msg += "key is pressed (value between 0 and 6), and displays the latest dice roll value it has received.\n"  
-	info_msg += "Connected to Spacebrew as: " + name + "\n"
-	info_msg += "IMPORTANT: don't shrink the Terminal window as it may cause app to crash (bug with curses lib)."  
-	stdscr.addstr(0, 0, info_msg.encode(code))
-	stdscr.refresh()
+    # create and load info message at the top of the terminal window
+    info_msg = "This is the pySpacebrew library custom data type example. It rolls the dice every time the enter or return\n"
+    info_msg += "key is pressed (value between 0 and 6), and displays the latest dice roll value it has received.\n"
+    info_msg += "Connected to Spacebrew as: " + name + "\n"
+    info_msg += "IMPORTANT: don't shrink the Terminal window as it may cause app to crash (bug with curses lib)."
+    stdscr.addstr(0, 0, info_msg.encode(code))
+    stdscr.refresh()
 
-	# update the location for the remote and local dice state 
-	pos_local = stdscr.getyx()[0] + 2
-	pos_remote = pos_local + 2
+    # update the location for the remote and local dice state
+    pos_local = stdscr.getyx()[0] + 2
+    pos_remote = pos_local + 2
 
-	# display the label for the remote and local dice roll states
-	stdscr.addstr(pos_local, 0, "local dice roll: ".encode(code), curses.A_BOLD)
-	stdscr.addstr(pos_remote, 0, "remote dice roll: ".encode(code), curses.A_BOLD)
+    # display the label for the remote and local dice roll states
+    stdscr.addstr(pos_local, 0, "local dice roll: ".encode(
+        code), curses.A_BOLD)
+    stdscr.addstr(pos_remote, 0, "remote dice roll: ".encode(
+        code), curses.A_BOLD)
 
-	# display the starting prompt next to local and remote dice state lines
-	stdscr.addstr(pos_local, pos_state, "waiting for first roll".encode(code))
-	stdscr.addstr(pos_remote, pos_state, "waiting for first roll".encode(code))
-	stdscr.refresh()
+    # display the starting prompt next to local and remote dice state lines
+    stdscr.addstr(pos_local, pos_state, "waiting for first roll".encode(code))
+    stdscr.addstr(pos_remote, pos_state, "waiting for first roll".encode(code))
+    stdscr.refresh()
 
-	# listen for keypresses and handle input
-	while 1:
-		c = stdscr.getch()
+    # listen for keypresses and handle input
+    while 1:
+        c = stdscr.getch()
 
-		if (c == 10 or c == 13): 
-			local_state = random.randint(1,6)
-			brew.publish("roll of the dice", local_state)
-			stdscr.addstr(pos_local, pos_state, (" " * 30).encode(code))
-			stdscr.addstr(pos_local, pos_state, str(local_state).encode(code))
+        if (c == 10 or c == 13):
+            local_state = random.randint(1, 6)
+            brew.publish("roll of the dice", local_state)
+            stdscr.addstr(pos_local, pos_state, (" " * 30).encode(code))
+            stdscr.addstr(pos_local, pos_state, str(local_state).encode(code))
 
-		stdscr.refresh()
+        stdscr.refresh()
 
 # closing out the app and returning terminal to old settings
 finally:
-	brew.stop()
-	curses.nocbreak()
-	stdscr.keypad(0)
-	curses.echo()
-	curses.endwin()
+    brew.stop()
+    curses.nocbreak()
+    stdscr.keypad(0)
+    curses.echo()
+    curses.endwin()
--- examples/range_client.py
+++ examples/range_client.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 import time
 import locale
@@ -21,10 +21,10 @@ name = "pyRange Example"
 server = "sandbox.spacebrew.cc"
 
 for cur_ele in sys.argv:
-	if "name" in cur_ele: 
-		name = cur_ele[5:]
-	if "server" in cur_ele: 
-		server = cur_ele[7:]
+    if "name" in cur_ele:
+        name = cur_ele[5:]
+    if "server" in cur_ele:
+        server = cur_ele[7:]
 
 # configure the spacebrew client
 brew = Spacebrew(name, server=server)
@@ -42,74 +42,81 @@ local_state = 500
 remote_state = 0
 
 # method that updates the range "bars" and value on the display
+
+
 def displayRange(value, source_line):
-	stdscr.addstr(source_line, pos_int, "     ".encode(code))
-	stdscr.addstr(source_line, pos_int, str(value).encode(code))
-	stdscr.addstr(source_line, pos_state, "".encode(code))
-	for i in range(100):
-		if (value / 10) > i:
-			stdscr.addstr(" ".encode(code), curses.A_STANDOUT)
-		else: 
-			stdscr.addstr(" ".encode(code))
-	stdscr.refresh()
+    stdscr.addstr(source_line, pos_int, "     ".encode(code))
+    stdscr.addstr(source_line, pos_int, str(value).encode(code))
+    stdscr.addstr(source_line, pos_state, "".encode(code))
+    for i in range(100):
+        if (value / 10) > i:
+            stdscr.addstr(" ".encode(code), curses.A_STANDOUT)
+        else:
+            stdscr.addstr(" ".encode(code))
+    stdscr.refresh()
 
 # function that handles the incoming spacebrew range messages
+
+
 def handleRange(value):
-	global code, stdscr
-	remote_state = int(value)
-	displayRange(remote_state, col_remote)
+    global code, stdscr
+    remote_state = int(value)
+    displayRange(remote_state, col_remote)
+
 
 # registering range handler method with appropriate subscription feed
 brew.subscribe("graph", handleRange)
 
 try:
-	brew.start()
+    brew.start()
 
-	# set app information message
-	info_msg =  "This is the pySpacebrew library range example. This app sends out a range value, between 0 and 1023. The value \n"
-	info_msg += "increases and decreases in response to '+'/'=' and `-`/'_' key presses. App also displays a range value received\n"  
-	info_msg += "from Spacebrew. Connected as: " + name + "\n"
-	info_msg += "IMPORTANT: don't shrink the Terminal window as it may cause app to crash (bug with curses lib)."  
-	stdscr.addstr(0, 0, info_msg.encode(code))
-	stdscr.refresh()
+    # set app information message
+    info_msg = "This is the pySpacebrew library range example. This app sends out a range value, between 0 and 1023. The value \n"
+    info_msg += "increases and decreases in response to '+'/'=' and `-`/'_' key presses. App also displays a range value received\n"
+    info_msg += "from Spacebrew. Connected as: " + name + "\n"
+    info_msg += "IMPORTANT: don't shrink the Terminal window as it may cause app to crash (bug with curses lib)."
+    stdscr.addstr(0, 0, info_msg.encode(code))
+    stdscr.refresh()
 
-	# update the location of the local and remote range linds
-	col_local = stdscr.getyx()[0] + 2
-	col_remote = col_local + 2
+    # update the location of the local and remote range linds
+    col_local = stdscr.getyx()[0] + 2
+    col_remote = col_local + 2
 
-	# display the label for the remote and local range states
-	stdscr.addstr(col_local, 0, "local range:  ".encode(code), curses.A_BOLD)
-	stdscr.addstr(col_remote, 0, "remote range: ".encode(code), curses.A_BOLD)
+    # display the label for the remote and local range states
+    stdscr.addstr(col_local, 0, "local range:  ".encode(code), curses.A_BOLD)
+    stdscr.addstr(col_remote, 0, "remote range: ".encode(code), curses.A_BOLD)
 
-	# display the starting state for remote and local range states
-	displayRange(local_state, col_local) 	
-	displayRange(remote_state, col_remote) 
-	stdscr.refresh()
+    # display the starting state for remote and local range states
+    displayRange(local_state, col_local)
+    displayRange(remote_state, col_remote)
+    stdscr.refresh()
 
-	# listen for keypresses and handle input
-	while 1:
-		c = stdscr.getch()
+    # listen for keypresses and handle input
+    while 1:
+        c = stdscr.getch()
 
-		new_data = False
-		if c == ord('+') or c == ord('='):
-			local_state += 10
-			if local_state > 1023: local_state = 1023
-			new_data = True
-		elif c == ord('-') or c == ord('_'):
-			local_state -= 10
-			if local_state < 0: local_state = 0
-			new_data = True
+        new_data = False
+        if c == ord('+') or c == ord('='):
+            local_state += 10
+            if local_state > 1023:
+                local_state = 1023
+            new_data = True
+        elif c == ord('-') or c == ord('_'):
+            local_state -= 10
+            if local_state < 0:
+                local_state = 0
+            new_data = True
 
-		if new_data:
-			brew.publish("slider", local_state)
-			displayRange(local_state, col_local)
+        if new_data:
+            brew.publish("slider", local_state)
+            displayRange(local_state, col_local)
 
-		stdscr.refresh()
+        stdscr.refresh()
 
 # closing out the app and returning terminal to old settings
 finally:
-	brew.stop()
-	curses.nocbreak()
-	stdscr.keypad(0)
-	curses.echo()
-	curses.endwin()
+    brew.stop()
+    curses.nocbreak()
+    stdscr.keypad(0)
+    curses.echo()
+    curses.endwin()
